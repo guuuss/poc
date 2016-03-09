7dd5b8fcea2bb73e544f61c0e7a36749bf7910fd
guus mulder <guuuss@live.nl>
2016-03-09 14:34:13 +0100

    front linked

---
+++ WebApplication1/front/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* App Module */
+
+var pocapp = angular.module('pocApp', [
+  'ngRoute',
+  'ngAnimate',
+]);
+
+pocapp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/products', {
+            templateUrl: 'partials/product-list.html',
+            controller: 'ProductListCtrl'
+        }).
+        when('/products/:productdetails/:productid', {
+            templateUrl: 'partials/product-detail.html',
+            controller: 'ProductDetailCtrl'
+        }).
+        when('/admin/:controlpanel', {
+            templateUrl: 'partials/adminpanel.html',
+            controller: 'AdminCtrl'
+        }).
+        when('/customer/:shoppingcart', {
+            templateUrl: 'partials/shoppingcart.html',
+            controller: 'shoppingcartCtrl'
+        }).
+        otherwise({
+            redirectTo: '/products'
+        });
+  }]);

d1a4d5ba9b91f6a5560b48fc02e4e898c05a3e50
Ryanne <ryanne-jansen@hotmail.com>
2016-03-09 12:37:21 +0100

    css + html
    lijst producten staat er nu in zonder koppeling

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -18,11 +18,13 @@ namespace WebApplication1
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/app.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/products.css"));
         }
     }
 }
